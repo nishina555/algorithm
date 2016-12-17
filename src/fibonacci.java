18e0622ada80ef405053abf9151e181887fe1e3a
nishina <n.toshi.1.014@gmail.com>
2016-12-17 22:24:32 +0900

    :sparkles: Add Fibonacci

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -2,4 +2,16 @@
  * Created by nishina on 2016/12/15.
  */
 public class Fibonacci {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i + "番目のフィボナッチ数列は" + fibonacci(i) + "です");
+        }
+    }
+    private static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacci(n - 2) + fibonacci(n - 1);
+        }
+    }
 }
