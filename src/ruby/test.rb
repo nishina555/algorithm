857dea16022cc1e8898befeb69404106a61cc345
nishina <n.toshi.1.014@gmail.com>
2016-12-19 22:44:32 +0900

    :sparkles: Add Ruby files

---
+++ src/ruby/test.rb
@@ -0,0 +1,3 @@
+class Test
+  print "hello world!"
+end
--- src/test.java
+++ src/test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String args[]) {
+        String[] array = new String[3];
+        array[0] = "apple";
+        array[1] = "pinapple";
+        System.out.println(array[0]);
+        System.out.println(array[1]);
 
     }
 }
