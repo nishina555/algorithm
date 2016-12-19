2aaea3d347b6b1d02a3a49294fa745180ede1864
nishina <n.toshi.1.014@gmail.com>
2016-12-20 00:02:13 +0900

    :sparkles: euclid_gcd by Ruby

--- src/ruby/euclid_gcd.rb
+++ src/ruby/euclid_gcd.rb
@@ -1,3 +1,21 @@
-class EuclidGcd
-
+def euclidGcd(x, y)
+  if y == 0 then
+    return x
+  else
+    return euclidGcd(y, x%y)
+  end
 end
+
+puts "整数値をスペース区切りで二つ入力してください"
+input = STDIN.gets.chomp.split( ).map(&:to_i)
+x = input[0]
+y = input[1]
+if x > y then
+  tmp = x
+  x = y
+  y = tmp
+end
+print "最大公約数は#{euclidGcd(x, y)}です"
+# print "最大公約数は", euclidGcd(x, y), "です"
+
+
--- src/ruby/test.rb
+++ src/ruby/test.rb
@@ -1,3 +1,9 @@
-class Test
-  print "hello world!"
+def printHello
+  print("Hello¥n")
 end
+
+printHello
+
+puts("整数値を入力してください")
+message = STDIN.gets.chomp
+print("あなたが入力したのは#{message}です")
