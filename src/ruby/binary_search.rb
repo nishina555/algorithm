5dc3159202cd0a2e8519712244af3a2c1dd6df1f
nishina <n.toshi.1.014@gmail.com>
2016-12-20 21:12:54 +0900

    :sparkles: Add binary_search by Ruby

---
+++ src/ruby/binary_search.rb
@@ -0,0 +1,40 @@
+def binarySearch(key, data)
+  pLeft = 0
+  pRight = data.length - 1
+  while pLeft <= pRight do
+    center = (pLeft + pRight) / 2
+    if data[center] == key then
+      return center
+    end
+    if data[center] < key then
+      pLeft = center + 1
+    else
+      pRight = center - 1
+    end
+  end
+  return -1
+end
+
+data = []
+10.times do |i|
+  data.push(i+1)
+end
+
+puts "数値を入力してください"
+searchNum = gets.chomp.to_i
+
+if searchNum > 10 || searchNum < 1 then
+  puts "1から10の間で入力してください"
+  exit
+else
+  puts searchNum
+end
+
+puts "バイナリーサーチ開始"
+
+position = binarySearch(searchNum, data)
+if position == -1 then
+  puts "#{searchNum}はみつかりませんでした"
+else
+  puts "#{searchNum}は#{position}番目にあります"
+end
